1f9bd40959e9d5883e75fa743da458e431f0730a
Meet Kansara <meetkansara05413@gmail.com>
2026-08-05 17:00:24 +0530

    Sales done

--- backend/sales/admin.py
+++ backend/sales/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Sales
+
+
+@admin.register(Sales)
+class SalesAdmin(admin.ModelAdmin):
+    list_display = (
+        "invoice_number",
+        "product",
+        "sale_date",
+        "quantity_sold",
+        "selling_price",
+        "discount",
+        "total_amount",
+    )
+
+    search_fields = (
+        "invoice_number",
+        "product__product_name",
+        "product__barcode",
+    )
+
+    list_filter = (
+        "sale_date",
+    )
+
+    list_select_related = (
+        "product",
+    )
+
+    ordering = (
+        "-sale_date",
+    )
